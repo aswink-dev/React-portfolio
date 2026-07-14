8c9db09d58aa68fe229c44a25da2898e423b12ff
= <aswinctm@gmail.com>
2026-07-14 22:48:28 +0530

    Integrate Microsoft Clarity

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Clarity from "@microsoft/clarity";
+
+Clarity.init("xmf3onzkig");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
